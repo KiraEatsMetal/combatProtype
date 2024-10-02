9473c2aadbe0da0feb7b6e3cfc411b662ac6fa99
KiraEatsMetal <kiraeatsmetal@gmail.com>
2024-10-02 10:31:56 -0700

    added a basic dodge and a dodge cooldown system

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -5,16 +5,31 @@ class Player extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
         scene.physics.add.existing(this);
         //set up parameters
-        this.moveSpeed = 1
+        this.moveSpeed = 8/10
         console.log('created player')
         console.log(texture)
+        this.direction = 0
+        this.dodgeDistance = 100
+        this.dodging = false
+        this.dodgeCooldown = 500
+        this.dodgeCool = 0
     }
     update(dt) {
-        if(true) {
+        //cooldown defensive ability
+        this.dodgeCool = Math.max(0, this.dodgeCool - dt)
+        //console.log(this.dodgeCool)
+
+        if(true) { //this part is placeholder for preventing movement while attacking
+            if(Phaser.Input.Keyboard.JustDown(keyDODGE) && this.dodgeCool == 0) {
+                this.x += this.dodgeDistance * this.direction
+                this.dodgeCool = this.dodgeCooldown
+            }
             if(keyLEFT.isDown) {
                 this.x -= this.moveSpeed * dt
+                this.direction = -1
             } else if(keyRIGHT.isDown) {
                 this.x += this.moveSpeed * dt
+                this.direction = 1
             }
         }
     }
